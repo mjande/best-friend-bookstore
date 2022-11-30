f710e4269a3dc37ebece847d855cf12aa294c304
mattand <mjande@umich.edu>
2022-11-30 15:36:34 -0500

    Add Add Cart button to BookCard

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,16 +1,17 @@
-import "../styles/Book.css";
+import "../styles/BookCard.css";
 
 const BookCard = ({ title, author, coverSrc }) => {
   return (
-    <article className="Book">
+    <article className="BookCard">
       <img
         src={`https://covers.openlibrary.org/b/id/${coverSrc}.jpg`}
         alt="Book cover"
       ></img>
-      <div className="Book-details">
+      <div className="BookCard-details">
         <h3>{title}</h3>
         <h4>by {author}</h4>
       </div>
+      <button>Add to Cart</button>
     </article>
   );
 };
