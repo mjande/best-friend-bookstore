d62751830516a8dee9c44459c36ddafae264be71
mattand <mjande@umich.edu>
2022-12-09 12:55:00 -0500

    Fix link in category cards

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -5,10 +5,7 @@ import "../styles/CategoryCard.css";
 const CategoryCard = ({ name }) => {
   return (
     <div className={`category ${name}`}>
-      <Link
-        to={process.env.PUBLIC_URL + `/books/subject/${name}`}
-        className="category-link"
-      >
+      <Link to={`/books/subject/${name}`} className="category-link">
         {kebabCaseToTitleCase(name)}
       </Link>
     </div>
